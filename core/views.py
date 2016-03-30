6b8820dba8fc6fbb4cc6c25c24f466b4c6b72cf6
Lucas Possatti <lucas_possatti@hotmail.com>
2016-03-30 18:43:06 -0300

    Create an index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 
-# Create your views here.
+
+def index(request):
+	return render(request, 'index.html')
