6baa3398461d950db02f5c047edc770524b6e5c7
Lucas Possatti <lucas_possatti@hotmail.com>
2016-03-31 16:44:23 -0300

    Create skill tree page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 app_name = 'core'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^skills/$', views.skills, name='skills'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render
 
 def index(request):
     return render(request, 'index.html')
+
+def skills(request):
+    return render(request, 'skills.html')
